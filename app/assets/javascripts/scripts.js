783cf0adefcf9ee1ab62175c20cab0de9e9995a6
robertSahm <robertsahm@gmail.com>
2012-07-26 16:08:41 -0700

    added info text areas for videos

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,13 @@
-
-
-
-
+$(function() {
+  $('a.videoClick').click(function() {
+    $('.playerWrapper').fadeIn(500);
+    $('.videoSelector').fadeOut(500);
+  });
+  $('a.backClick').click(function() {
+    $('.videoSelector').fadeIn(500);
+    $('.playerWrapper').fadeOut(500);
+  });
+});
 
 
 

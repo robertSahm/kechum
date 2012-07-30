8ba034fb6093a032044bdf6828e92bca89f1445f
robertSahm <robertsahm@gmail.com>
2012-07-29 17:51:31 -0700

    experimenting with front page slider animations

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,5 @@
+
+// fades in/out video player 
 $(function() {
   $('a.videoClick').click(function() {
     $('.playerWrapper, .playerHide').fadeIn(500);
@@ -7,6 +9,15 @@ $(function() {
     $('.videoSelector').fadeIn(500);
     $('.playerWrapper, .playerHide').fadeOut(500);
   });
+
+  $('.teamHover').hover(function() {
+    $('.teamBlock').fadeIn(500);
+    $('.teamBlock a').click(function() {
+      $(this).fadeOut(500);
+      $('.teamHover').fadeOut(500);
+    });
+  });
+
 });
 
 

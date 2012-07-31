2c2e09388a47c668ad056dfed7048464b3099b50
robertSahm <robertsahm@gmail.com>
2012-07-30 22:11:33 -0700

    basic animations working, no shadows yet

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -9,19 +9,116 @@ $(function() {
     $('.videoSelector').fadeIn(500);
     $('.playerWrapper, .playerHide').fadeOut(500);
   });
-
-  $('.teamHover').click(function() {
-    $('.teamBlock').toggle(500);
-    
-  });
-
 });
 
+$(function() {
+  $('.teamHover').hover(function(){
+    $('.teamBlock').stop(true, true).animate({
+      top:'-=28', 
+      left:'-=87'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.teamBlock').stop(true, true).animate({
+      top:'+=28', 
+      left: '+=87'
+    },{queue:false,duration:300});
+  });
+});
 
+$(function() {
+  $('.televisionHover').hover(function(){
+    $('.televisionBlock').stop(true, true).animate({
+      top:'-=27', 
+      left:'-=87'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.televisionBlock').stop(true, true).animate({
+      top:'+=27', 
+      left: '+=87'
+    },{queue:false,duration:300});
+  });
+});
 
+$(function() {
+  $('.yardsaleHover').hover(function(){
+    $('.yardsaleBlock').stop(true, true).animate({
+      top:'-=27', 
+      left:'-=87'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.yardsaleBlock').stop(true, true).animate({
+      top:'+=27', 
+      left: '+=87'
+    },{queue:false,duration:300});
+  });
+});
 
+$(function() {
+  $('.weatherHover').hover(function(){
+    $('.weatherBlock').stop(true, true).animate({
+      top:'-=28', 
+      left:'-=86'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.weatherBlock').stop(true, true).animate({
+      top:'+=28', 
+      left: '+=86'
+    },{queue:false,duration:300});
+  });
+});
 
+$(function() {
+  $('.contactHover').hover(function(){
+    $('.contactBlock').stop(true, true).animate({
+      top:'-=28', 
+      left:'-=86'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.contactBlock').stop(true, true).animate({
+      top:'+=28', 
+      left: '+=86'
+    },{queue:false,duration:300});
+  });
+});
 
+$(function() {
+  $('.clientHover').hover(function(){
+    $('.clientBlock').stop(true, true).animate({
+      top:'-=28', 
+      left:'-=86'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.clientBlock').stop(true, true).animate({
+      top:'+=28', 
+      left: '+=86'
+    },{queue:false,duration:300});
+  });
+});
 
+$(function() {
+  $('.filmHover').hover(function(){
+    $('.filmBlock').stop(true, true).animate({
+      top:'-=28', 
+      left:'-=86'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.filmBlock').stop(true, true).animate({
+      top:'+=28', 
+      left: '+=86'
+    },{queue:false,duration:300});
+  });
+});
 
-
+$(function() {
+  $('.digitalHover').hover(function(){
+    $('.digitalBlock').stop(true, true).animate({
+      top:'-=28', 
+      left:'-=86'
+    },{queue:false,duration:300});
+  }, function(){
+    $('.digitalBlock').stop(true, true).animate({
+      top:'+=28', 
+      left: '+=86'
+    },{queue:false,duration:300});
+  });
+});

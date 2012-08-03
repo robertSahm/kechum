e2a832fec04cab42d496ffde8d2dda470b562761
Jon Code <programcodex@gmail.com>
2012-08-03 14:44:47 -0400

    root to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,6 @@
 class HomeController < ApplicationController
   
   def index
-    @users = User.order('id DESC')
-    
-  end
-  
-  def help
-    
     
   end
   
@@ -14,32 +8,6 @@ class HomeController < ApplicationController
     
     
   end
-
-  def about
-    
-    
-  end
-  
-  def channel
-    @micropost = current_user.microposts.build if signed_in?
-    @feed_items = Micropost.all
-    
-    
-    
-  end
-  
-  def learn
-    
-    
-  end
-  
-  def news
-    
-    
-  end
-  
-
-
 
 end
 

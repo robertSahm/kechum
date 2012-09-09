7025a00e78123b03dda27abdc23200026dffde06
Jon Code <programcodex@gmail.com>
2012-09-09 01:18:14 -0400

    remove the shadow random, new permalink method

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,20 +1,16 @@
 class Video < ActiveRecord::Base
   attr_accessible :brand, :description, :director, :permalink, :project, :type_of, :url
 
-  before_create :create_permalink
+  after_create :create_permalink
   
   # code that only allows film, tlevsion, digital or, all as type_of 
   
   private
     
     def create_permalink
-      # geneerate a 24 digit random number
-      number = ""
-      24.times do
-        n = rand(9)
-        number += n.to_s
-      end
-      self.permalink = number
+      number = "546708330517471623061836"
+      index = self.id
+      self.permalink = "#{index}#{number}"
+      self.save!
     end
-
 end

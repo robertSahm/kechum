60790d8304e585eadd44285417c294bfadeba788
Jon Code <programcodex@gmail.com>
2012-09-11 22:08:29 -0400

    seed file + type_of validation

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,14 +1,15 @@
 class Video < ActiveRecord::Base
-  attr_accessible :brand, :description, :director, :permalink, :project, :type_of, :url
+  attr_accessible :brand, :description, :director, :permalink, :project, :type_of, :url, :posterframe, :producer
 
   after_create :create_permalink
   
-  # code that only allows film, tlevsion, digital or, all as type_of 
+  # code that only allows film, televsion, digital or, all as type_of 
+  validates_inclusion_of :type_of, :in => ['film', 'television', 'digital'], :message => "attribute :type_of must be 'film, television, or digital' "
   
   private
     
     def create_permalink
-      number = "546708330517471623061836"
+      number = "546708330517"
       index = self.id
       self.permalink = "#{index}#{number}"
       self.save!
